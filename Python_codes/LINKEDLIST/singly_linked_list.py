e8d6d3cedcdf33b7ab3f7712c33b5101d1a1c8bf
shindetejasm <shindetejasm@gmail.com>
2024-05-25 14:39:35 +0530

    SinglyLinkedList all routines completed

--- Python_codes/LINKEDLIST/singly_linked_list.py
+++ Python_codes/LINKEDLIST/singly_linked_list.py
@@ -50,25 +50,77 @@ class singlyLinkedList:
         run.next = new_node
        
     def insert_before(self,existing_data,new_data)->bool:
-            pass
-       
-    def pop_start(self):
-             pass
-  
-    def pop_end(self):
-             pass
+        run = self.head_node
+        while run.next != None :
+               if run.next.data == existing_data:
+                      break
+               run = run.next
+        else:
+               raise ValueError(f"{existing_data} not found")
+        # create new node
+        new_node = Node(new_data)
+        new_node.next = run.next
+        run.next = new_node
+        return True
+    
+    
     def get_start(self):
-             pass
+        if self.head_node.next is None :
+                raise ValueError("Can not get the start as list is empty")
+        return self.head_node.next.data
   
     def get_end(self):
-             pass
+        if self.head_node.next is None :
+                raise ValueError("Can not get the start as list is empty")
+        run =  self.head_node
+        while  run.next is not None:
+                run = run.next
+        return run.data
+
+    def pop_start(self):
+        if self.head_node.next is None : 
+                raise ValueError("list is empty!!")
+        data = self.head_node.next.data 
+        self.head_node.next =self.head_node.next.next
+        return data
     
+    def pop_end(self):
+        if self.head_node.next is None:
+              raise ValueError("list is empty !!!")
+        run =  self.head_node
+        while  run.next.next is not None:
+              run = run.next
+        data = run.next.data
+        run.next = run.next.next
+        return data
+
     def remove_start(self):
-             pass
+        if self.head_node.next is None : 
+                raise ValueError("list is empty!!")
+        self.head_node.next =self.head_node.next.next
+
     
     def remove_end(self):
-             pass
-    def show(self):
+        if self.head_node.next is None:
+         raise ValueError("list is empty !!!")
+        run =  self.head_node
+        while  run.next.next is not None:
+              run = run.next
+        run.next = run.next.next
+
+    def remove_data(self,new_data):
+        if self.head_node.next is None:
+         raise ValueError("list is empty !!!")
+        run =  self.head_node
+        while  run.next is not None and run.next.data != new_data:
+              run = run.next
+        run.next = run.next.next
+
+
+    def show(self,s:str):
+        if type(s) != str:
+              raise TypeError("arg not a string")
+        print(s)    
         print("[START] ->",end= ' ')
         trav =  self.head_node.next
         while trav is not None:
@@ -76,8 +128,7 @@ class singlyLinkedList:
                 trav=trav.next
         print("[END]")
 
-    def remove_data(self,new_data):
-             pass
+   
 
 
 def main():
@@ -90,6 +141,23 @@ def main():
         L.insert_end(60)
         L.insert_end(70)
         L.insert_end(80)
+        L.insert_end(101)
+        L.insert_before(15,45)
+        L.show("after adding data")
+        data1 = L.get_start()
+        data2 = L.get_end()
+        L.show("after get data")
+        print(f"get data ata start {data1}")
+        print(f"get data ata start {data2}")
+        data3 = L.pop_start()
+        L.show("after pop_start() data")
+        data4 = L.pop_end()
+        L.show("after pop_end() data")
+        L.remove_end()
+        L.show("after L.remove_end() data")
+        L.remove_data(60)
+        L.show("after L.remove_data(15) data")
+        L.remove_start()
+        L.show("after L.remove_start() data")
 
-        L.show()
 main()
